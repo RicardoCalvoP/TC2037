5781ab217c203fc4de3e164247b7b34e286b1523
RicardoCalvoP <a01028889@tec.mx>
2024-05-16 19:02:18 -0600

    v1 first integrated functions

--- Actividad_Integradora/main.ex
+++ Actividad_Integradora/main.ex
@@ -14,7 +14,7 @@ defmodule Project do
     # Create & Write top html file
     write_html_start(html_file)
 
-    read_file(file)
+    # read_file(file)
 
     # Write closure tags and close file
     write_html_end(html_file)
@@ -27,10 +27,13 @@ defmodule Project do
 
   # Function yo call to read the calling file line by line
   def read_file(file, html_file) do
-    line = File.read!(file)
+    lines = File.stream!(file)
+    #
+    # Results are going to be list of tuples [{token, result}]
+    # Enum.map(lines, &find_coincidences(&1, []))
     # Check elements for future results
     # Save results in list of tuples
-    write_results(html_file, results)
+    # write_results(html_file, results)
   end
 
   # Main function to put together  all of the html doc with results
@@ -40,7 +43,7 @@ defmodule Project do
 
   # Function to write the beginning of html structure
   def write_html_start(file) do
-    File.write!(new_name, """
+    File.write!(file, """
     <!DOCTYPE html>
     <html lang="en">
     <head>
@@ -68,19 +71,72 @@ defmodule Project do
   # Finding coincidences  Functions
   # --------------------------------
 
+  def find_coincidences(line) do
+    result = []
+    is_reserved_word(line, result)
+    # is_variable(line)
+    # is_comment(line)
+    # is_string(line)
+    # is_operator(line)
+  end
+
   # -----------------------
   # Defining rule Functions
   # -----------------------
 
-  # def is_reserved_word(string),
-  #  do:
-  #    Enum.map(
-  #     &Regex.scan(~r/^defmodule$|^def$|^defp$|^do$|^end$|^false$|^true$|^cond$|^case$/, string)
-  #  )
+  def is_reserved_word(string, result) do
+    coincidence = Regex.run(~r/^(defmodule|def|defp|do|end|false|true|cond|case)/, string)
 
-  # def is_variable(string), do: Enum.map(&Regex.scan(~r/^[a-zA-Z]\w*/, &1))
+    cond do
+      coincidence ->
+        result = [[:reserved_word, hd(coincidence)] | result]
+        new_string = String.split_at(string, String.length(hd(coincidence)))
+        is_reserved_word(elem(new_string, 1), result)
 
-  # def is_comment(string), do: Enum.map(&Regex.scan(~r/s?\#.*/, &1))
+      string == "" ->
+        result
+
+      true ->
+        is_func(string, result)
+    end
+  end
+
+  def is_func(string, result) do
+    coincidence = Regex.run(~r/^[a-z]\w*\((\w*(\,\s(\w*|\d*))*?)*?\)/, string)
+
+    cond do
+      coincidence ->
+        result = [[:function, hd(coincidence)] | result]
+        new_string = String.split_at(string, String.length(hd(coincidence)))
+        is_reserved_word(elem(new_string, 1), result)
+
+      string == "" ->
+        result
+
+      true ->
+        is_variable(string, result)
+    end
+  end
+
+  def is_variable(string, result) do
+    coincidence = Regex.run(~r/^[a-z]\w*/, string)
+
+    cond do
+      coincidence ->
+        result = [[:variable, hd(coincidence)] | result]
+        new_string = String.split_at(string, String.length(hd(coincidence)))
+        is_reserved_word(elem(new_string, 1), result)
+
+      string == "" ->
+        result
+
+      true ->
+        new_string = String.split_at(string, 1)
+        is_space(string, result)
+    end
+  end
+
+  # def is_comment(string), do: Regex.scan(~r/s?\#.*/, string)
 
   # def is_string(string), do: Enum.map(&Regex.scan(~r/\".*\"/, &1))
 
@@ -92,4 +148,22 @@ defmodule Project do
   # def is_container(string), do: Enum.map(&Regex.scan(~r/\(|\)|\{|\}|\[|\]/, &1))
 
   # def is_regular_expression(string), do: Enum.map(&Regex.scan(~r/\~r.*\//, &1))
+
+  def is_space(string, result) do
+    coincidence = Regex.run(~r/^\s/, string)
+
+    cond do
+      coincidence ->
+        result = [[:space, hd(coincidence)] | result]
+        new_string = String.split_at(string, String.length(hd(coincidence)))
+        is_reserved_word(elem(new_string, 1), result)
+
+      string == "" ->
+        result
+
+      true ->
+        new_string = String.split_at(string, 1)
+        is_reserved_word(elem(new_string, 1), result)
+    end
+  end
 end
